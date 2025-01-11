88a8c2ecf17f1e6c284237f366c6733a654c7cfe
gbpii <99564332+gbpii@users.noreply.github.com>
2025-01-11 09:36:19 +0300

    Home page
    - Login page design added.
    - Login button component added.
    
    - ts:48:40

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,35 @@
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
-    <div className="text-zinc-500 font-bold">
-      Hİ!
-      <br/>
-      <Button 
-        size="lg"
-        variant="outline"
-        className="bg-zinc-900"
-      >
-        Click me!
-      </Button>
-    </div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-zinc-400 to-zinc-800">
+      <div className="space-y-6 text-center">
+        <h1 className={cn(
+          "text-6xl font-semibold text-white drop-shadow-md",
+          font.className
+          )}>
+          Secure <span className="text-transparent bg-clip-text bg-gradient-to-r from-zinc-100 to-zinc-400">Next.js</span> Auth
+        </h1>
+        <p className="text-lg text-zinc-400">
+          Authentication system build with Next.js and Tailwind CSS
+        </p>
+          <div>
+            <LoginButton>
+              <Button variant="secondary" size="lg">
+                Sign In
+              </Button>
+            </LoginButton>
+          </div>
+      </div>
+    </main>
   );
 }
