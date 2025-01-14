93f573ca6c29f30604e560aacf3f786412a38197
gbpii <99564332+gbpii@users.noreply.github.com>
2025-01-14 18:10:20 +0300

    chore: debug log removal.

--- src/actions/logout.ts
+++ src/actions/logout.ts
@@ -6,7 +6,6 @@ import { DEFAULT_LOGIN_ADRESS } from "@/lib/routes";
 export const logout = async () => {
 
   // optional: doing server stuff before logout.
-  console.log("logout called");
   await signOut({ redirectTo: DEFAULT_LOGIN_ADRESS, });
 
 };
