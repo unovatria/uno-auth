b7b8e090d72a36e1617b578c5ca0e5a59e201bfc
gbpii <99564332+gbpii@users.noreply.github.com>
2025-01-14 18:06:44 +0300

    chore: (tsconfig.json - package.json - next.config.mjs) updates.

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,31 @@ export default nextConfig;
 
 
 /*
-On Consumer projects you have to add:
+On consumer projects you have to add: (Projects that use this project as module)
+
+/// next.config.js
+
+const path = require('path');
+
+module.exports = {
+  transpilePackages: ['your-package-name'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'node_modules/your-package-name/dist'),
+    };
+    return config;
+  },
+};
+
+/// next.config.mjs
 
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-/** @type {import('next').NextConfig} /<-- ADD "*" in here when in use.
+/** @type {import('next').NextConfig} / <-- ADD "*" in here when in use.
 const nextConfig = {
   transpilePackages: ['your-package-name'],
   webpack: (config) => {
@@ -25,6 +42,5 @@ const nextConfig = {
 };
 
 export default nextConfig;
-
 
 */
